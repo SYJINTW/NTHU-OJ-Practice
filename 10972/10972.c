ed76ef87f962460a25b7509889fafa5cbbefba03
SYJINTW <yuanjiun911@outlook.com>
2021-03-26 01:03:52 +0800

    finish 10972

--- 10972/10972.c
+++ 10972/10972.c
@@ -5,25 +5,27 @@
 #include <string.h>
 
 int fakeStack[300];
-int now = 0;
+int now = 0; // point out the next pos the stack can store
 
-char S[300];
-char B[300];
+char S[300]; // store the user data
+int B[300]; // store if the char in S[] can print out
 
-int lastoneisp = 1;
-int firstp = 0;
+int lastoneisp = 1; // if the last char is '(', then the flag turn to '0'
+int firstp = 0; // if this is the first '(' in the expression, then the flag is '0'
 
 int main()
 {   
     scanf("%s", S);
     for(int i = 0; S[i]!='\0'; i++)
     {
+        // if the char is Variable, then must change two flags
         if(S[i]=='A'||S[i]=='B'||S[i]=='C'||S[i]=='D')
         {
             B[i] = 1;
             lastoneisp = 1;
             firstp = 1;
         }
+        // if the char is operator, then must change two flags
         else if(S[i]=='|'||S[i]=='&')
         {
             B[i] = 1;
@@ -32,34 +34,37 @@ int main()
         }
         else if(S[i]=='(')
         {
+            // detect () -> NULL
             if(S[i+1]==')')
             {
                 B[i] = 0;
                 fakeStack[now++] = 0; 
             }
+            // detect (A) -> A
             else if((S[i+1]=='A'||S[i+1]=='B'||S[i+1]=='C'||S[i+1]=='D') && S[i+2]==')')
             {
                 B[i] = 0;
                 fakeStack[now++] = 0;   
             }
+            // if '(' is in the first of the expression then won't print it
             else
             {
                 B[i] = firstp*lastoneisp;
                 fakeStack[now++] = firstp*lastoneisp;
-                //printf("%d\n", fakeStack[now-1]); 
                 lastoneisp = 0;
             }
         }
+        // Use fakeStack to check if the ')' can print out, depend on it's '('
         else if(S[i]==')')
         {
-            //printf("%d\n", fakeStack[now-1]);
             B[i] = fakeStack[--now];
         }
     }
 
+    // print out the char if it's B[i] not '0'
     for(int i = 0; S[i]!='\0'; i++)
     {
         if(B[i]!=0)printf("%c", S[i]);
     }
-    //printf("\n");
+    return 0;
 }
